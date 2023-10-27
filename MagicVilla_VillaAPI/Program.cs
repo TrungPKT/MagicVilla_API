a73ebb5e115f77f88204b25ad030c4a0529901b1
TrungPKT <trung.phankimthanh@gmail.com>
2023-10-27 13:25:31 +0700

    23, ReturnHttpNotAcceptable 406 check accept type, return format json, xml. Add support for xml format.

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -2,7 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers(option =>
+{
+    // 23, API has default type of Json. GET req. has default accept for Json. Set following property to turn on Acccept type check.
+    // 23, IF API does not return the correct Accept type format, it will return 406NotAcceptable.
+    option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();    // 23, Add XML format for response.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

ad77b4987deaa9c51c6ba88a694004e7a0ab43f2
TrungPKT <trung.phankimthanh@gmail.com>
2023-10-28 18:18:05 +0700

    29, Add new properties to Villa and VillaDTO, add packages EF.SqlServer, EF.Tools

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -1,10 +1,23 @@
-﻿namespace MagicVilla_VillaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models
 {
     public class Villa
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        // 29,
+        public string Details { get; set; }
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        // 29, Image maybe stored in a different DB (Azure storage)
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         // 9, A column in the DB which we do not want to expose it to the end user
         public DateTime CreatedDate { get; set; }
+        // 29,
+        public DateTime UpdatedDate { get; set; }
     }
 }

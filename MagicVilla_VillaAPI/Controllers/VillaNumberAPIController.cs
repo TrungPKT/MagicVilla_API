102fc53545c8ae35113693ad46756d2a54bca53f
TrungPKT <trung.phankimthanh@gmail.com>
2023-10-30 14:49:40 +0700

    41, VillaNumberAPI (not done)

---
+++ MagicVilla_VillaAPI/Controllers/VillaNumberAPIController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaNumberAPI")]
+    [ApiController]
+    public class VillaNumberAPIController : ControllerBase
+    {
+        protected APIResponse _response;
+        private readonly IVillaNumberRepository _dbVillaNumber;
+        private readonly IMapper _mapper;
+
+        public VillaNumberAPIController(IVillaNumberRepository dbVillaNumber, IMapper mapper)
+        {
+            _response = new APIResponse();
+            _dbVillaNumber = dbVillaNumber;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetVillaNumbers()
+        {
+            try
+            {
+                IEnumerable<VillaNumber> villaNumberList = await _dbVillaNumber.GetAllAsync();
+
+                _response.Result = _mapper.Map<List<VillaNumberDTO>>(villaNumberList);
+                _response.StatusCode = HttpStatusCode.OK;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpGet("{villaNo:int}", Name = "GetVillaNumber")]
+        public async Task<ActionResult<APIResponse>> GetVillaNumber(int villaNo)
+        {
+            try
+            {
+                if (villaNo <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages = new List<string> { "villaNo must be greater than 0" };
+
+                    return BadRequest(_response);
+                }
+
+                var villaNumber = await _dbVillaNumber.GetAsync(villaNumber => villaNumber.VillaNo == villaNo);
+
+                if (villaNumber == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages = new List<string>() { "villaNo not found" };
+
+                    return NotFound(_response);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = villaNumber;
+                
+                return _response;
+            }
+            catch (Exception ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+    }
+}
